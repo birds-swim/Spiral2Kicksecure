edf7f35bc819554740daa953f5f9e4b57035861c
birds.swim <the.greatsword@pm.me>
2023-04-22 15:51:20 -0400

    Update more_software.sh

--- more_software.sh
+++ more_software.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Installs more software that I thought was cool or useful.
+# Additional packages range from more security tools/services to bash tweaks from https://terminalsare.sexy/
 
 sudo apt update
 sudo apt -Vy full-upgrade
