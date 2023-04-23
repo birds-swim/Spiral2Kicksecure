d3e40441bb7b477f2325b425d0d44dafd1d343fb
birds.swim <the.greatsword@pm.me>
2023-04-23 12:34:21 -0400

    Update add_sec_pkgs.sh

--- add_sec_pkgs.sh
+++ add_sec_pkgs.sh
@@ -5,7 +5,11 @@ sudo apt update
 sudo apt -Vy full-upgrade
 
 # Installs more security-centric software that I thought was cool or useful.
-sudo apt -Vy --no-install-recommends install lynis opensnitch
+sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan
+
+# Either Kicksecure's LKRG or Debian's hardening-runtime package
+#sudo apt -Vy install lkrg
+#sudo apt -Vy install hardening-runtime
 
 #
 
