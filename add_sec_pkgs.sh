41bb40bf2d32eb87127edf56c81d8fddfa0ccc2c
birds.swim <the.greatsword@pm.me>
2023-04-23 13:55:18 -0400

    Update add_sec_pkgs.sh

--- add_sec_pkgs.sh
+++ add_sec_pkgs.sh
@@ -8,10 +8,12 @@ sudo apt -Vy full-upgrade
 # I personally don't think debsecan is very useful to the average home user, as debsecan reports on information that you really don't have
 # any control over.  It's a nice information tool, but unless you're gonna help with security patches or be proactive with this information,
 # I'm not sure what you would do with debsecan. Leaving it in here because someone might find it useful.
-sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan tiger rkhunter auditd
+sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan tiger rkhunter auditd fail2ban endlessh needrestart apt-listbugs 
 # Samhain is a File Integrity Monitoring (FIM) service and Host-based Intrusion Detection System (HIDS)
 # https://www.comparitech.com/net-admin/file-integrity-monitoring-tools/
 # https://www.la-samhna.de/samhain/
+###-----Samhain didn't work on my Spiral2Kick during the early stages of installation and setup of all software considered.
+###-----It failed to install with a dpkg error, so I removed Samhain. I do not have time to toubleshoot why it didn't work.
 #sudo apt -V install samhain
 
 # Either Kicksecure's LKRG or Debian's hardening-runtime package
