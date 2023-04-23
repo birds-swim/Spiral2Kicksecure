6c0ebc53e321b2077dc6f4f1db2ce3aa1c56b129
birds.swim <the.greatsword@pm.me>
2023-04-23 13:02:52 -0400

    Update Terminal_Apps_Reminder.sh

--- Terminal_Apps_Reminder.sh
+++ Terminal_Apps_Reminder.sh
@@ -2,5 +2,8 @@
 
 # Remind the user of CLI apps that have been installed on the system.
 echo "neofetch hstr fzf mc bpytop lynis deb-get micro"
+echo ""
+echo "SECURITY"
+echo "lynis opensnitch usbguard debsecan samhain tiger rkhunter auditd"
 
 exit
