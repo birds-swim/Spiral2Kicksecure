8fa337876120bf57bf03daa741b5df370ad4b17e
birds.swim <the.greatsword@pm.me>
2023-04-23 15:49:53 -0400

    Update add_sec_pkgs.sh

--- add_sec_pkgs.sh
+++ add_sec_pkgs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+###---------------------------------INTRODUCTION-----------------------------###
+# Installs more security-centric software that I thought was cool or useful.
+
 # Update the system
 sudo apt update
 sudo apt -Vy full-upgrade
 
-# Installs more security-centric software that I thought was cool or useful.
 # I personally don't think debsecan is very useful to the average home user, as debsecan reports on information that you really don't have
 # any control over.  It's a nice information tool, but unless you're gonna help with security patches or be proactive with this information,
 # I'm not sure what you would do with debsecan. Leaving it in here because someone might find it useful.
