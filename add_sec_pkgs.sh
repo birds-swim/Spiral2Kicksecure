6793ae07f0bad54f0b4c025f4b0098ab04446b5f
birds.swim <the.greatsword@pm.me>
2023-04-23 12:44:59 -0400

    Update add_sec_pkgs.sh

--- add_sec_pkgs.sh
+++ add_sec_pkgs.sh
@@ -5,6 +5,9 @@ sudo apt update
 sudo apt -Vy full-upgrade
 
 # Installs more security-centric software that I thought was cool or useful.
+# I personally don't think debsecan is very useful to the average home user, as debsecan reports on information that you really don't have
+# any control over.  It's a nice information tool, but unless you're gonna help with security patches or be proactive with this information,
+# I'm not sure what you would do with debsecan. Leaving it in here because someone might find it useful.
 sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan
 
 # Either Kicksecure's LKRG or Debian's hardening-runtime package
