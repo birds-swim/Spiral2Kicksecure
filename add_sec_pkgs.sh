3bef08929cdb9b629d4eb28e6e1f9a4f1eb033f5
birds.swim <the.greatsword@pm.me>
2023-04-23 13:01:46 -0400

    Update add_sec_pkgs.sh

--- add_sec_pkgs.sh
+++ add_sec_pkgs.sh
@@ -8,7 +8,10 @@ sudo apt -Vy full-upgrade
 # I personally don't think debsecan is very useful to the average home user, as debsecan reports on information that you really don't have
 # any control over.  It's a nice information tool, but unless you're gonna help with security patches or be proactive with this information,
 # I'm not sure what you would do with debsecan. Leaving it in here because someone might find it useful.
-sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan
+sudo apt -Vy --no-install-recommends install lynis opensnitch usbguard debsecan samhain tiger rkhunter auditd
+# Samhain is a File Integrity Monitoring (FIM) service and Host-based Intrusion Detection System (HIDS)
+# https://www.comparitech.com/net-admin/file-integrity-monitoring-tools/
+# https://www.la-samhna.de/samhain/
 
 # Either Kicksecure's LKRG or Debian's hardening-runtime package
 #sudo apt -Vy install lkrg
